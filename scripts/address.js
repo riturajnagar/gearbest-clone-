043687487e656d66121b268a4f04dde8006ae5c6
roni8420 <101327545+roni8420@users.noreply.github.com>
2022-07-21 23:28:07 +0530

    Add files via upload

---
+++ scripts/address.js
@@ -0,0 +1,49 @@
+
+
+function User(f,l,al,al2,c,s,ci,zi,p,e){
+
+    this.first = f;
+    this.last = l;
+    this.addLine = al;
+    this.adddLine2 = al2;
+    this.country = c;
+    this.state = s;
+    this.city = ci;
+    this.zipCode = zi;
+    this.phone = p;
+    this.email = e;
+
+}
+
+let confirm = (e) =>{
+
+    e.preventDefault()
+
+    let form = document.getElementById('formFill');
+
+    let first = form.name.value;
+    let last = form.lastName.value;
+    let add1 = form.address.value;
+    let add2 = form.address2.value;
+    let country = form.country.value;
+    let state = form.state.value;
+    let city = form.city.value;
+    let pin = form.pin.value;
+    let phone = form.phone.value;
+    let email = form.email.value;
+
+
+    let user = new User(first,last,add1,add2,country,state,city,pin,phone,email)
+
+    // let data = JSON.parse(localStorage.getItem("users")) || [];
+
+    // data.push(user1)
+
+    localStorage.setItem("users",JSON.stringify(user))
+
+    console.log(user)
+
+    alert("Address Successfully Saved")
+    // window.location.href = "./payment.html"
+
+}
---
+++ scripts/payment.js
@@ -0,0 +1,33 @@
+
+let append2 = ()=> {
+
+    let data = JSON.parse(localStorage.getItem("users")) || [];
+    let append2= document.getElementById("append2")
+    append2.innerHTML = null;
+
+    
+
+        let add = document.createElement("h4");
+        add.innerText = data.addLine
+        let country = document.createElement("h4");
+        country.innerText = data.country
+        let state = document.createElement("h4");
+        state.innerText = data.state
+
+        append2.append(add,country,state)
+        
+
+
+}
+append2()
+
+let append1 = () => {
+
+    let orderId = document.getElementById("append1")
+    let id =  document.createElement("h4");
+    id.innerText = Math.floor(Math.random() * 1000000000000) + 1;
+
+    orderId.append(id)
+
+}
+append1()
